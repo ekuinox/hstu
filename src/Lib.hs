332c93b3dd59c73d6c163980569badf4c7c58c8f
ekuinox <depkey@me.com>
2020-03-10 02:21:42 +0900

    httpbin.org/ipにリクエストしてる

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,25 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
     ) where
 
 import Control.Monad
+import Data.Aeson
+import Network.HTTP.Simple
+
+data HttpbinIpResponse = HttpbinIpResponse { origin :: String } deriving Show
+
+instance FromJSON HttpbinIpResponse where
+    parseJSON (Object v) = HttpbinIpResponse <$> v .: "origin"
+    parseJSON _ = mempty
 
 someFunc :: IO ()
-someFunc = forM_ (map fizzbuzz [0..100]) $ \result -> 
-    putStrLn result
+someFunc = do
+    res <- httpLBS "https://httpbin.org/ip"
+    let ip = decode (getResponseBody res) :: Maybe HttpbinIpResponse
+    case ip of
+        Nothing -> putStrLn "failure"
+        Just ip -> putStrLn (origin ip)
 
 fizzbuzz :: Integer -> String
 fizzbuzz num =
